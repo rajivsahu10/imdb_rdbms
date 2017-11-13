874a35bbb767ecbf3855ee042c66bfcc41ea6d57
Rajiv Sahu <rajivsahu10@gmail.com>
2017-11-14 01:33:24 +0530

    initial commit

---
+++ src/main/java/com/rajiv/imdb/ImdbRdbmsApplication.java
@@ -0,0 +1,12 @@
+package com.rajiv.imdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImdbRdbmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImdbRdbmsApplication.class, args);
+	}
+}
